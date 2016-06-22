fb7d932911d4a48989c280ccdc5ddd04e346bfaa
sternold <tobi-pepijn@hotmail.com>
2016-06-22 14:05:29 +0200

    GUI changed to ASP.NET; IO class library added.

--- LPWF.IO/Properties/AssemblyInfo.cs
+++ LPWF.IO/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LPWF.GUI")]
+[assembly: AssemblyTitle("LPWF.IO")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LPWF.GUI")]
+[assembly: AssemblyProduct("LPWF.IO")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("069c25f8-250c-46b4-9b3e-89b4eadfcab8")]
+[assembly: Guid("a41bab24-38ef-4f04-beaa-7c94ef687be2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
